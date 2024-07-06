be826c483c362b5492a79e13ed125045ff4d038f
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-07-06 13:49:12 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""
-This module contains a function that queries the Reddit API to get the
-number of subscribers for a given subreddit.
-"""
-
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for
-    a given subreddit. If the subreddit is invalid, returns 0.
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/your_username)'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        return response.json().get('data').get('subscribers')
     else:
         return 0
